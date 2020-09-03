96b085933d741ad9144ef8bfd8943ec63a7b0ee8
横山ざき <zakysteinfeld@outlook.com>
2020-09-03 15:27:06 +0900

    add new package - BCrypt

--- Controllers/DefaultTalentHunterRouteController.cs
+++ Controllers/DefaultTalentHunterRouteController.cs
@@ -1,4 +1,9 @@
 using System.Net;
+using AutoMapper;
+using Emveep.TalentHunter.API.Controllers.Model;
+using Emveep.TalentHunter.API.Data.Interface;
+using Emveep.TalentHunter.API.Dtos;
+using Emveep.TalentHunter.API.Models.auth;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Emveep.TalentHunter.API.Controllers
@@ -7,6 +12,30 @@ namespace Emveep.TalentHunter.API.Controllers
     [ApiController]
     public class DefaultTalentHunterRouteController : ControllerBase
     {
+        #region Properties
+        public TalentHunterResponse talentHunterResp { get; private set; }
+        public TalentHunterCreateDtos talentHunterCreateDtos { get; private set; }
+        #endregion
+
+        #region Operator        
+
+        private readonly ITalentHunterRepo THRepo;
+        private readonly IMapper _mapper;
+
+        #endregion
+
+        #region Constructor
+
+        public DefaultTalentHunterRouteController(ITalentHunterRepo repository, IMapper mapper)
+        {
+            THRepo = repository;
+            _mapper = mapper;
+        }
+
+        #endregion
+
+        #region Implementations
+            
         [HttpGet]
         public ContentResult Get()
         {
@@ -15,6 +44,31 @@ namespace Emveep.TalentHunter.API.Controllers
                 StatusCode = (int)HttpStatusCode.OK,
                 Content = "<html><header><title>Welcome! - Emveep Talent Hunter API</title></header><body><h1>Congratulations! You connected to this API.</h1></body></html>"
             };
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult <TalentHunterDtos> GetProfilebyId(int id) 
+        {
+            TalentHunterRequest requestDomain;
+            requestDomain = MappingRequest(id);
+            
+            var resultStatus = THRepo.GetProfilebyId(requestDomain);
+
+            if (resultStatus != null)
+            {
+                return Ok(_mapper.Map<TalentHunterDtos>(resultStatus));
+            }
+            return NotFound();
         }    
+
+        #endregion
+
+        private TalentHunterRequest MappingRequest(int id)
+        {
+            TalentHunterRequest requestDomain = new TalentHunterRequest();
+            requestDomain.id = id;
+
+            return requestDomain;
+        }
     }
 }
